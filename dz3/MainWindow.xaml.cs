9fd09becc7ed944b7712e55ac6071429d095992e
Bohdanna Kalynchuk <dana398076@gmail.com>
2023-03-27 20:09:14 +0300

    2 commit

---
+++ dz3/MainWindow.xaml.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dz3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string path = "";
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Text file(*.txt)|*.txt";
+            
+            if (openFile.ShowDialog() == true)
+            {
+                
+                    StreamReader stream = File.OpenText(openFile.FileName);
+                Notepad.Text = stream.ReadToEnd();
+                path = openFile.FileName;
+                stream.Close();
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter= "Text file(*.txt)|*.txt";
+            
+           if(saveFile.ShowDialog()==true) {
+                StreamWriter stream = new StreamWriter(saveFile.FileName);
+                stream.Write(Notepad.Text);
+                path = "";
+                stream.Close();
+            }
+        }
+
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            if (path != "")
+            {
+
+                MessageBoxResult result = MessageBox.Show("Changes will not be saved ", "Massage", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (result == MessageBoxResult.No)
+                {
+                    return;
+                }
+
+            }
+            Notepad.Clear();
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            if(Notepad.TextWrapping == TextWrapping.Wrap)
+            {
+                window1.Ch(true, Notepad.FontSize/10);
+            }
+            else window1.Ch(false, Notepad.FontSize/10);
+
+            if ( window1.ShowDialog()==true)
+                {
+                if (window1.WrapChecked)
+                {
+                    Notepad.TextWrapping = TextWrapping.Wrap;
+                }
+                else Notepad.TextWrapping = TextWrapping.NoWrap;
+                if(window1.FontChecked == 1)
+                {
+                    Notepad.FontSize = 10;
+                }
+                else if(window1.FontChecked == 2)
+                {
+                    Notepad.FontSize = 20;
+                }
+                else if (window1.FontChecked == 3)
+                {
+                    Notepad.FontSize = 30;
+                }
+                else if (window1.FontChecked == 4)
+                {
+                    Notepad.FontSize = 40;
+                }
+                else if (window1.FontChecked == 5)
+                {
+                    Notepad.FontSize = 50;
+                }
+            }
+        }
+
+        private void SelectAll_Click(object sender, RoutedEventArgs e)
+        {
+            Notepad.SelectAll();
+           
+        }
+
+        private void Paste_Click(object sender, RoutedEventArgs e)
+        {
+            Notepad.Paste();
+        }
+
+        private void Cut_Click(object sender, RoutedEventArgs e)
+        {
+            Notepad.Cut();
+        }
+
+        private void Copy_Click(object sender, RoutedEventArgs e)
+        {
+            Notepad.Copy();
+        }
+    }
+}
---
+++ dz3/Window1.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static dz3.MainWindow;
+
+namespace dz3
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        
+        public bool WrapChecked = false;
+        public int FontChecked = 0;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        public void Ch(bool _WrapChecked, double _NotepadFontSize)
+        {
+            WrapChecked = _WrapChecked;
+            if (_WrapChecked)
+            {
+                WrapTextCheckbox.IsChecked = true;
+            }
+            else WrapTextCheckbox.IsChecked = false;
+            if (_NotepadFontSize == 1)
+            {
+                VerySmall.IsChecked = true;
+            }
+            else if (_NotepadFontSize == 2)
+            {
+                Small.IsChecked = true;
+            }
+            else if (_NotepadFontSize == 3)
+            {
+                Normal.IsChecked = true;
+            }
+            else if (_NotepadFontSize == 4)
+            {
+                Big.IsChecked = true;
+            }
+            else if (_NotepadFontSize == 5)
+            {
+                VeryBig.IsChecked = true;
+            }
+
+        }
+
+        private void WrapTextCheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            WrapChecked = true;
+          
+           
+        }
+
+        private void WrapTextCheckbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            WrapChecked = false;
+            
+            
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            this.Close();
+        }
+
+        private void VerySmall_Checked(object sender, RoutedEventArgs e)
+        {
+            FontChecked = 1;
+        }
+
+        private void Small_Checked(object sender, RoutedEventArgs e)
+        {
+            FontChecked = 2;
+        }
+
+        private void Normal_Checked(object sender, RoutedEventArgs e)
+        {
+            FontChecked = 3;
+        }
+
+        private void Big_Checked(object sender, RoutedEventArgs e)
+        {
+            FontChecked = 4;
+        }
+
+        private void VeryBig_Checked(object sender, RoutedEventArgs e)
+        {
+            FontChecked = 5;
+        }
+    }
+}
